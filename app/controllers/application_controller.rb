ef682449e04a6ecc151bf306951ae0f20d0c56cd
shlee <shlee@neocyon.com>
2016-03-21 01:19:49 +0900

    First Commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == 'admin' && password == 'admin'
+    end
+  end
+end
